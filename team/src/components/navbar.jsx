2341472dad33407dc1c2891bf3cdbf1a37eacf75
pozdniakova <xpozdniakova@stuba.sk>
2025-10-16 16:13:37 +0200

    add mobile version

--- team/src/components/navbar.jsx
+++ team/src/components/navbar.jsx
@@ -1,17 +1,38 @@
 import "../styles/navbar.css"
 import logo from "../imgs/team_logo.png";
+import burger from "../imgs/icons-menu.png";
+import closeIcon from "../imgs/icons-close.png"; 
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <img src={logo} alt="logo" className="navbar-logo" />
+
+        {/* Desktop menu */}
         <ul className="navbar-menu">
           <li><a href="#about" className="active">O PROJEKTE</a></li>
           <li><a href="#sprints">ŠPRINTY</a></li>
           <li><a href="#team">TÍM</a></li>
           <li><a href="#documents">DOKUMENTY</a></li>
           <li><a href="#contact">KONTAKTY</a></li>
+        </ul>
+
+        {/* Burger button */}
+         <button className="burger" onClick={() => setIsOpen(!isOpen)}>
+          <img src={isOpen ? closeIcon : burger} alt="menu" className="icon" />
+        </button>
+
+        {/* Mobile menu */}
+        <ul className={`menu ${isOpen ? "open" : ""}`}>
+          <li><a href="#about" onClick={() => setIsOpen(false)}>O PROJEKTE</a></li>
+          <li><a href="#sprints" onClick={() => setIsOpen(false)}>ŠPRINTY</a></li>
+          <li><a href="#team" onClick={() => setIsOpen(false)}>TÍM</a></li>
+          <li><a href="#documents" onClick={() => setIsOpen(false)}>DOKUMENTY</a></li>
+          <li><a href="#contact" onClick={() => setIsOpen(false)}>KONTAKTY</a></li>
         </ul>
       </div>
     </nav>
